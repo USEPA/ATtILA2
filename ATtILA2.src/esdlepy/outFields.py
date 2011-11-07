483b347f00c8a27b1a753821af52fee556f95d7a
debert@D2626UDEBERT001.aa.ad.epa.gov <debert@D2626UDEBERT001.aa.ad.epa.gov>
2011-11-07 15:35:54 -0800

    field parameters

---
+++ ATtILA2.src/esdlepy/outFields.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 7, 2011
+
+@author: debert
+'''
+
+
+fieldParameters = {"LandCoverProportions":["p","", "FLOAT", 6, 1],
+                   "RiparianProportions":[]}
+
+optionalFieldParameters = {"LandCoverProportions":[["LC_OVERLAP", "FLOAT", 6, 1]]}
+
+def getFieldParametersFromFilename(fileName=None):
+    """ """
+
+def getOptionalFieldParametersFromFilename(fileName=None):
