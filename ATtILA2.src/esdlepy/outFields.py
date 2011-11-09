2491b064601a913a1bf15665368fc95892abe110
Michael Jackson <jackson.michael@epa.gov>
2011-11-08 20:46:46 -0800

    New method and associated dictionary to get field override key from file path

--- ATtILA2.src/esdlepy/outFields.py
+++ ATtILA2.src/esdlepy/outFields.py
@@ -6,20 +6,40 @@
 
 import os
 import sys
+import lcc
+
+# Land Cover Proportions(lcp)
+lcpMetricName = "LandCoverProportions"
+lcpFieldPrefix = "p"
+lcpOverlapName = "LC_OVERLAP"
+
+# Riparian Proportions(rp)
+rpMetricName = "RiparianProportions"
+rpFieldPrefix = "r"
+rpOverlapName = "R_OVERLAP"
+
+# Field type defaults
+defaultDecimalFieldType = "FLOAT"
+
 
 # Required field parameter info
-fieldParameters = {"LandCoverProportions":["p","", "FLOAT", 6, 1],
-                   "RiparianProportions":[]}
+fieldParameters = {lcpMetricName:[lcpFieldPrefix,"", defaultDecimalFieldType, 6, 1],
+                   rpMetricName:[rpFieldPrefix,"", defaultDecimalFieldType, 6, 1]}
 
 # Optional field parameter info
-optionalFieldParameters = {"LandCoverProportions":[["LC_OVERLAP", "FLOAT", 6, 1]],
-                           "RiparianProportions":[["R_OVERLAP", "FLOAT", 6, 1]]}
+optionalFieldParameters = {lcpMetricName:[[lcpOverlapName, defaultDecimalFieldType, 6, 1]],
+                           rpMetricName:[[rpOverlapName, defaultDecimalFieldType, 6, 1]]}
+
+
+# field Override keys
+fieldOverrideKeys = {lcpMetricName: lcc.constants.XmlAttributeLcpField,
+                     rpMetricName: lcc.constants.XmlAttributeRpField}
 
 
 def getFieldParametersFromFilePath(filePath=None, delimiter="_"):
     """ Parses full file path to look up required field parameters
         
-        No arguments are required.  If filePath is not given, it uses sys.argv[0] (the full path to executed script)
+        No arguments are required.  If filePath is not given, it uses sys.argv[0] (full path to executed script)
         
         filePath:  full file path from which basename is split by delimiter, first item in split is used
         delimiter: character(s) by which basename is split
@@ -29,7 +49,7 @@ def getFieldParametersFromFilePath(filePath=None, delimiter="_"):
     if not filePath:
         filePath = sys.argv[0]
     
-    key = os.path.basename(filePath).split(delimiter)[0]
+    key = getKeyFromFilePath(filePath, delimiter)
     
     return fieldParameters[key]
         
@@ -37,7 +57,7 @@ def getFieldParametersFromFilePath(filePath=None, delimiter="_"):
 def getOptionalFieldParametersFromFilePath(filePath=None, delimiter="_"):
     """ Parses full file path to look up optional field parameters
         
-        No arguments are required.  If filePath is not given, it uses sys.argv[0] (the full path to executed script)
+        No arguments are required.  If filePath is not given, it uses sys.argv[0] (full path to executed script)
         
         filePath:  full file path from which basename is split by delimiter, first item in split is used
         delimiter: character(s) by which basename is split
@@ -47,7 +67,35 @@ def getOptionalFieldParametersFromFilePath(filePath=None, delimiter="_"):
     if not filePath:
         filePath = sys.argv[0]
     
-    key = os.path.basename(filePath).split(delimiter)[0]
+    key = getKeyFromFilePath(filePath, delimiter)
     
     return optionalFieldParameters[key]
+
+
+def getFieldOverrideKeyFromFilePath(filePath=None, delimiter="_"):
+    """ Parse full file path to look up Field Override Key
+    
+        No arguments are required.  If filePath is not given, it uses sys.argv[0] (full path to executed script)
+        
+        filePath:  full file path from which basename is split by delimiter, first item in split is used
+        delimiter: character(s) by which basename is split
+
+    """
+    
+    if not filePath:
+        filePath = sys.argv[0]
+    
+    key = getKeyFromFilePath(filePath, delimiter)
+    
+    return fieldOverrideKeys[key]
+
+
+def getKeyFromFilePath(filePath, delimiter):
+    """ Parses full file path to return embedded key.
+    
+        Expected format:  <key><delimiter>anything
+    """
+
+    return os.path.basename(filePath).split(delimiter)[0]
+
 
