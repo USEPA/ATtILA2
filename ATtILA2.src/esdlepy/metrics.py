0ab98488efa8877fbd6ad1e87449bc4e209c37d1
Michael Jackson <jackson.michael@epa.gov>
2011-11-18 14:54:11 -0800

    Created new metric subpackage in esdlepy

--- ATtILA2.src/esdlepy/metrics.py
+++ ATtILA2.src/esdlepy/metrics.py
@@ -4,7 +4,3 @@ Created on Nov 17, 2011
 @author: mjacks07
 '''
 
-
-# Reporting Units
-noFeaturesMessage = "Dataset exists, but there are no features (zero rows)"
-
---
+++ ATtILA2.src/esdlepy/metrics/constants.py
@@ -0,0 +1,13 @@
+'''
+Created on Nov 18, 2011
+
+@author: mjacks07
+
+Constants specific to metric tools  
+'''
+
+
+
+# Reporting units
+noFeaturesMessage = "Dataset exists, but there are no features (zero rows)"
+
