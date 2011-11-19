57922573f4b393c8ee36eac611287397ae9bb15f
Michael Jackson <jackson.michael@epa.gov>
2011-11-18 18:49:51 -0800

    Constants copied to constants.py module

--- ATtILA2.src/esdlepy/metrics/constants.py
+++ ATtILA2.src/esdlepy/metrics/constants.py
@@ -6,8 +6,16 @@ Created on Nov 18, 2011
 Constants specific to metric tools  
 '''
 
-
-
-# Reporting units
+# Common
 noFeaturesMessage = "Dataset exists, but there are no features (zero rows)"
+inputIdFieldTypes = ["SmallInteger", "Integer", "String"]
+userOption = "User Defined"
+optionalFieldsName = "Optional Fields"
+descriptionDelim = "  "
+qaCheckName = "QACHECK"
+metricAddName = "METRICADD"
+qaCheckDescription = "{0}{1}-  Quality Assurance Checks".format(qaCheckName, descriptionDelim)
+metricAddDescription = "{0}{1}-  Area for all land cover classes".format(metricAddName, descriptionDelim)
 
+# Land Cover Proportions
+lcpMetricDescription = "{0}" + descriptionDelim + "[{1}]  {2}"
