e74318d1cb73a3a1a2970d6108003d6922f063d9
Michael Jackson <jackson.michael@epa.gov>
2011-11-22 18:32:58 -0800

    Template for updating optional enivronment overrides for input raster

--- ATtILA2.src/esdlepy/metrics/validation/LandCoverProportions.py
+++ ATtILA2.src/esdlepy/metrics/validation/LandCoverProportions.py
@@ -263,7 +263,17 @@ class ToolValidator:
         # Remove required on optional fields
         self.optionalFieldsParameter.clearMessage()
 
-
+        # Check if input raster is defined
+        if self.inRasterParameter.value:
+            
+            # Update Processing cell size if empty
+            if not self.processingCellSizeParameter.value and not self.processingCellSizeParameter.hasError():
+                cellSize = 30
+                self.processingCellSizeParameter.value = cellSize
+            
+            # Update Snap Raster Parameter if it is empty
+            if not self.snapRasterParameter.value and not self.inRasterParameter.hasError():
+                self.snapRasterParameter.value = str(self.inRasterParameter.value)
 
 
         
