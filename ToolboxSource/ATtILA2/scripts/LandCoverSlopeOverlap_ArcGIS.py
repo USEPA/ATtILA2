9b82fa72db8c1fd92d08cc0db33f2f19e8a4c5fb
don ebert <ebert.donald@epa.gov>
2012-03-19 08:06:55 -0700

    setting up inputs for ag on steep slopes metric

--- ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
+++ ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
@@ -33,3 +33,54 @@ def main(argv):
     Processing_cell_size = arcpy.GetParameterAsText(7)
     Snap_raster = arcpy.GetParameterAsText(8)
     Optional_field_groups = arcpy.GetParameterAsText(9)
+    
+    # the variables row and rows are initially set to None, so that they can
+    # be deleted in the finally block regardless of where (or if) script fails
+    outTable_row, outTable_rows = None, None
+    tabAreaTable_row, tabAreaTable_rows = None, None
+    
+    # get current snap environment to restore at end of script
+    tempEnvironment0 = env.snapRaster
+    
+    
+    try:
+        print "hello world"
+        
+        
+    except arcpy.ExecuteError:
+        arcpy.AddError(arcpy.GetMessages(2))
+        
+    except:
+        import traceback
+        # get the traceback object
+        tb = sys.exc_info()[2]
+        tbinfo = traceback.format_tb(tb)[0]
+        
+        # Concatenate information together concerning the error into a message string
+        
+        pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info()[1])
+        msgs = "ArcPy ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+    
+        # Return python error messages for use in script tool
+        
+        arcpy.AddError(pymsg)
+        arcpy.AddError(msgs)
+        
+    finally:
+        # delete cursor and row objects to remove locks on the data
+        if outTable_row: del outTable_row
+        if outTable_rows: del outTable_rows
+        if tabAreaTable_rows: del tabAreaTable_rows
+        if tabAreaTable_row: del tabAreaTable_row
+            
+        # restore the environments
+        env.snapRaster = tempEnvironment0
+        
+        # return the spatial analyst license    
+        arcpy.CheckInExtension("spatial")
+        
+        print "Finished."
+    
+    
+if __name__ == "__main__":
+    main(sys.argv)
