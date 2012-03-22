c1157bed01cb1c7e9fc25cecda233c5ccbc5b589
don ebert <ebert.donald@epa.gov>
2012-03-22 15:23:21 -0700

    added code to create the input grid for the ag on steep slopes calculation.

--- ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
+++ ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
@@ -15,6 +15,7 @@
 
 import arcpy, os, sys
 from arcpy import env
+from arcpy.sa import *
 from pylet import lcc
 from ATtILA2.metrics import fields as outFields
 
@@ -29,10 +30,12 @@ def main(argv):
     Input_land_cover_grid = arcpy.GetParameterAsText(2)
     lccFilePath = arcpy.GetParameterAsText(4)
     Metrics_to_run = arcpy.GetParameterAsText(5)
-    Output_table = arcpy.GetParameterAsText(6)
-    Processing_cell_size = arcpy.GetParameterAsText(7)
-    Snap_raster = arcpy.GetParameterAsText(8)
-    Optional_field_groups = arcpy.GetParameterAsText(9)
+    Input_slope_grid = arcpy.GetParameterAsText(6)
+    Input_slope_threshold_value = arcpy.GetParameterAsText(7)
+    Output_table = arcpy.GetParameterAsText(8)
+    Processing_cell_size = arcpy.GetParameterAsText(9)
+    Snap_raster = arcpy.GetParameterAsText(10)
+    Optional_field_groups = arcpy.GetParameterAsText(11)
     
     # the variables row and rows are initially set to None, so that they can
     # be deleted in the finally block regardless of where (or if) script fails
@@ -41,10 +44,42 @@ def main(argv):
     
     # get current snap environment to restore at end of script
     tempEnvironment0 = env.snapRaster
+    # set the snap raster environment so the AG on Steep Slopes grid aligns with land cover grid cell boundaries
+    env.snapRaster = Snap_raster
+    env.cellSize = Processing_cell_size
     
     
     try:
-        print "hello world"
+        # XML Land Cover Coding file loaded into memory
+        lccObj = lcc.LandCoverClassification(lccFilePath)
+        # Get the lccObj values dictionary to determine if a grid code is to be included in the effective reporting unit area calculation
+        lccValuesDict = lccObj.values
+        # get the frozenset of excluded values (i.e., values not to use when calculating the reporting unit effective area)
+        excludedValues = lccValuesDict.getExcludedValueIds()
+        
+        # Generate the slope X land cover gird where areas below the threshold slope are
+        # set to the value 'Maximum Land Cover Class Value + 1'.
+        LCGrid = Raster(Input_land_cover_grid)
+        SLPGrid = Raster(Input_slope_grid)
+        AreaBelowThresholdValue = int(LCGrid.maximum + 1)
+        where_clause = "VALUE >= "+Input_slope_threshold_value
+        SLPxLCGrid = Con(SLPGrid, LCGrid, AreaBelowThresholdValue, where_clause)
+        
+        # see if user wants to save the secondary/intermediate grid products
+        SLPxLCGrid.save(arcpy.CreateUniqueName("slxlc"))
+
+        # if certain land cover codes are tagged as 'excluded = TRUE', generate grid where land cover codes are 
+        # preserved for areas coincident with steep slopes, areas below the slope threshold are coded with the 
+        # AreaBelowThresholdValue except for where the land cover code is included in the excluded values list.
+        # In that case, the excluded land cover values are maintained in the low slope areas.
+        
+        if excludedValues:
+            # build a whereClause string (e.g. "VALUE = 11 or VALUE = 12") to identify where on the land cover grid excluded values occur
+            whereExcludedClause = "VALUE = " + " or VALUE = ".join([str(item) for item in excludedValues])
+
+            excludedValuesGrid = Con(LCGrid, LCGrid, SLPxLCGrid, whereExcludedClause)
+            excludedValuesGrid.save(arcpy.CreateUniqueName("xval"))
+        
         
         
     except arcpy.ExecuteError:
@@ -80,6 +115,14 @@ def main(argv):
         arcpy.CheckInExtension("spatial")
         
         print "Finished."
+        
+
+def ParseCheckboxSelections(selectionsString):
+    """ Returns a list of the items selected by the user.
+        The expected input is a string with the following format: 'item<two spaces>description';'item<two spaces>description';'item...'
+        e.g., the string, 'for  [pfor] Forest';'wetl  [pwetl]  wetland', becomes the list, ['for','wetl']
+    """
+    return map((lambda splitItemAndDesc: splitItemAndDesc.split('  ')[0]), selectionsString.replace("'","").split(';'))
     
     
 if __name__ == "__main__":
