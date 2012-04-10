6166ff3981e51e380133a45e2071eaed1b5c9e90
Michael Jackson <jackson.michael@epa.gov>
2012-04-09 18:53:43 -0700

    Updated metric constants

--- ToolboxSource/ATtILA2/ATtILA2/metrics/constants.py
+++ ToolboxSource/ATtILA2/ATtILA2/metrics/constants.py
@@ -6,22 +6,33 @@ Created on Nov 18, 2011
 Constants specific to metric tools  
 '''
 
-# Common
+# Common Variables
 tbxSourceFolderName = "ToolboxSource"
 noFeaturesMessage = "Dataset exists, but there are no features (zero rows)"
 inputIdFieldTypes = ["SmallInteger", "Integer", "String"]
 userOption = "User Defined"
 optionalFieldsName = "Additional Options"
 descriptionDelim = "  -  "
-qaCheckName = "QAFIELDS"
-metricAddName = "AREAFIELDS"
 areaFieldParameters = ["_A","DOUBLE",15,0]
-qaCheckDescription = "{0}{1}Add Quality Assurance Fields".format(qaCheckName, descriptionDelim)
-metricAddDescription = "{0}{1}Add Area Fields for All Land Cover Classes".format(metricAddName, descriptionDelim)
 srcFolderSuffix = ".src"
 tbxFileSuffix = ".tbx"
 tbxFileDelim = "__"
 tbxSriptToolDelim = "#"
+metricDescription = "{0}" + descriptionDelim + "[{1}]  {2}"
+qaCheckName = "QAFIELDS"
+metricAddName = "AREAFIELDS"
+intermediateName = "INTERMEDIATES"
+qaCheckDescription = "{0}{1}Add Quality Assurance Fields".format(qaCheckName, descriptionDelim)
+metricAddDescription = "{0}{1}Add Area Fields for All Land Cover Classes".format(metricAddName, descriptionDelim)
+intermediateDescription = "{0}{1}Retain Intermediate Layers Generated During Metric Calculation".format(intermediateName, descriptionDelim)
 
-# Land Cover Proportions
-lcpMetricDescription = "{0}" + descriptionDelim + "[{1}]  {2}"
+# Metric Names Used for Lookup
+lcpMetricName = "LandCoverProportions"
+rpMetricName = "RiparianProportions"
+lcsoMetricName = "LandCoverSlopeOverlap"
+
+# Land Cover Proportions Specific Variables
+lcpOptionalFieldsFilter = [qaCheckDescription, metricAddDescription]
+
+# Land Cover and Slope Overlap
+lcsoOptionalFieldsFilter = [qaCheckDescription, metricAddDescription, intermediateDescription]
