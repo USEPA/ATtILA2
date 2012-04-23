c5acad919708123b339bd81e64a01e7ba95fd3cf
Michael Jackson <jackson.michael@epa.gov>
2012-04-23 16:51:50 -0700

    The bulk of executable LandCoverSlopeeOverlap_ArcGIS script was moved to ATtILA2.metrics.common

--- ToolboxSource/ATtILA2/ATtILA2/metrics/common.py
+++ ToolboxSource/ATtILA2/ATtILA2/metrics/common.py
@@ -8,13 +8,74 @@ import sys
 
 import arcpy
 from arcpy import env
+from arcpy.sa import *
 
 from pylet import arcpyutil
 from pylet import lcc
 
+
 from ATtILA2.metrics import fields as outFields
 from ATtILA2.metrics import constants as metricConstants
 from ATtILA2.metrics import common
+
+###########################################################################################################
+#Land Cover on Slope Proportions (Land Cover Slope Overlap)
+###########################################################################################################
+
+def landCoverOnSlopeProportions(inReportingUnitFeature, reportingUnitIdField, inLandCoverGrid, lccFilePath, 
+                                metricsToRun, inSlopeGrid, inSlopeThresholdValue, outTable, processingCellSize, 
+                                snapRaster, optionalFieldGroups):
+    """ Land Cover on Slope Proportions (Land Cover Slope Overlap)"""
+    
+    # Check out any necessary licenses
+    arcpy.CheckOutExtension("spatial")
+
+    
+    # XML Land Cover Coding file loaded into memory
+    lccObj = lcc.LandCoverClassification(lccFilePath)
+    
+    SLPxLCGrid = getIntersectOfGrids(lccObj,inLandCoverGrid, inSlopeGrid, inSlopeThresholdValue)
+    
+    # save the file if intermediate products outputs are checked
+    #SLPxLCGrid.save(arcpy.CreateUniqueName("slxlc"))
+    
+    
+    
+
+def getIntersectOfGrids(lccObj,inLandCoverGrid, inSlopeGrid, inSlopeThresholdValue):
+        
+    # Generate the slope X land cover gird where areas below the threshold slope are
+    # set to the value 'Maximum Land Cover Class Value + 1'.
+    LCGrid = Raster(inLandCoverGrid)
+    SLPGrid = Raster(inSlopeGrid)
+    AreaBelowThresholdValue = int(LCGrid.maximum + 1)
+    where_clause = "VALUE >= " + inSlopeThresholdValue
+    SLPxLCGrid = Con(SLPGrid, LCGrid, AreaBelowThresholdValue, where_clause)
+    
+    # Get the lccObj values dictionary to determine if a grid code is to be included in the effective reporting unit area calculation    
+    lccValuesDict = lccObj.values
+    
+    # get the frozenset of excluded values (i.e., values not to use when calculating the reporting unit effective area)
+    excludedValues = lccValuesDict.getExcludedValueIds()
+        
+    # if certain land cover codes are tagged as 'excluded = TRUE', generate grid where land cover codes are 
+    # preserved for areas coincident with steep slopes, areas below the slope threshold are coded with the 
+    # AreaBelowThresholdValue except for where the land cover code is included in the excluded values list.
+    # In that case, the excluded land cover values are maintained in the low slope areas.
+    if excludedValues:
+        # build a whereClause string (e.g. "VALUE = 11 or VALUE = 12") to identify where on the land cover grid excluded values occur
+        whereExcludedClause = "VALUE = " + " or VALUE = ".join([str(item) for item in excludedValues])
+    
+        SLPxLCGrid = Con(LCGrid, LCGrid, SLPxLCGrid, whereExcludedClause)
+    
+    return SLPxLCGrid
+
+
+
+
+###########################################################################################################
+#Land Cover Proportions
+###########################################################################################################
 
 def landCoverProportions(inReportingUnitFeature, reportingUnitIdField, inLandCoverGrid, lccFilePath, metricsToRun, outTable, processingCellSize, snapRaster, optionalFieldGroups) :
     """ Land Cover Proportion Metrics """   
--- ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
+++ ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
@@ -1,79 +1,20 @@
-# LandCoverSlopeOverlap_ArcGIS.py
-# Michael A. Jackson, jackson.michael@epa.gov, majgis@gmail.com
-# Donald W. Ebert, ebert.donald@epa.gov
-# 2012-03-15
-""" Land Cover Slope Overlap Metrics
+""" Land Cover on Slope Proportion Metrics
 
-    DESCRIPTION
-    
-    ARGUMENTS
-    
-    REQUIREMENTS
-    Spatial Analyst Extension
+    This script is associated with an ArcToolbox script tool
 
 """
-from pylet.arcpyutil import parameters
-
-import arcpy
 import sys
-from arcpy.sa import *
-from pylet import lcc
+from pylet.arcpyutil import parameters
+from ATtILA2 import metrics
 
-
-# Check out any necessary licenses
-arcpy.CheckOutExtension("spatial")
 
 def main(argv):
-    """ Start Here """
     
+    # Script arguments
     inputArguments = parameters.getParametersAsText([0])
     
-    landCoverOnSlopeProportions(*inputArguments)
+    metrics.common.landCoverOnSlopeProportions(*inputArguments)
 
-def landCoverOnSlopeProportions(inReportingUnitFeature, reportingUnitIdField, inLandCoverGrid, lccFilePath, 
-                                metricsToRun, inSlopeGrid, inSlopeThresholdValue, outTable, processingCellSize, 
-                                snapRaster, optionalFieldGroups):
-
-    # XML Land Cover Coding file loaded into memory
-    lccObj = lcc.LandCoverClassification(lccFilePath)
-    
-    SLPxLCGrid = getIntersectOfGrids(lccObj,inLandCoverGrid, inSlopeGrid, inSlopeThresholdValue)
-    
-    # save the file if intermediate products outputs are checked
-    #SLPxLCGrid.save(arcpy.CreateUniqueName("slxlc"))
-    
-    
-
-def getIntersectOfGrids(lccObj,inLandCoverGrid, inSlopeGrid, inSlopeThresholdValue):
-    
-    # Get the lccObj values dictionary to determine if a grid code is to be included in the effective reporting unit area calculation    
-    lccValuesDict = lccObj.values
-    
-    # get the frozenset of excluded values (i.e., values not to use when calculating the reporting unit effective area)
-    excludedValues = lccValuesDict.getExcludedValueIds()
-    
-    # Generate the slope X land cover gird where areas below the threshold slope are
-    # set to the value 'Maximum Land Cover Class Value + 1'.
-    LCGrid = Raster(inLandCoverGrid)
-    SLPGrid = Raster(inSlopeGrid)
-    AreaBelowThresholdValue = int(LCGrid.maximum + 1)
-    where_clause = "VALUE >= " + inSlopeThresholdValue
-    SLPxLCGrid = Con(SLPGrid, LCGrid, AreaBelowThresholdValue, where_clause)
-    
-    # if certain land cover codes are tagged as 'excluded = TRUE', generate grid where land cover codes are 
-    # preserved for areas coincident with steep slopes, areas below the slope threshold are coded with the 
-    # AreaBelowThresholdValue except for where the land cover code is included in the excluded values list.
-    # In that case, the excluded land cover values are maintained in the low slope areas.
-    
-    if excludedValues:
-        # build a whereClause string (e.g. "VALUE = 11 or VALUE = 12") to identify where on the land cover grid excluded values occur
-        whereExcludedClause = "VALUE = " + " or VALUE = ".join([str(item) for item in excludedValues])
-    
-        SLPxLCGrid = Con(LCGrid, LCGrid, SLPxLCGrid, whereExcludedClause)
-    
-    return SLPxLCGrid
-
-    
     
 if __name__ == "__main__":
     main(sys.argv)
