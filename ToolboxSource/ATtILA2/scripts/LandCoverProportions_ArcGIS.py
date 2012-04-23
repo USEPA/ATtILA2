c7bb24540f498627beb36c7852d9d78940d918cc
Michael Jackson <jackson.michael@epa.gov>
2012-04-23 16:25:13 -0700

    Refactoring of scripts before moving functions to a common location

--- ToolboxSource/ATtILA2/scripts/LandCoverProportions_ArcGIS.py
+++ ToolboxSource/ATtILA2/scripts/LandCoverProportions_ArcGIS.py
@@ -3,9 +3,8 @@
     This script is associated with an ArcToolbox script tool
     
 """
-
+from pylet.arcpyutil import parameters
 import arcpy
-import os
 import sys
 from arcpy import env
 from pylet import lcc
@@ -13,34 +12,18 @@ from ATtILA2.metrics import fields as outFields
 from ATtILA2.metrics import constants as metricConstants
 from pylet import arcpyutil
 
+from ATtILA2.metrics import common
+
 def main(argv):
     """ Start Here """
     
     # Script arguments
-    inReportingUnitFeature = arcpy.Describe(arcpy.GetParameter(0)).catalogPath # Access of catalogPath needed for ArcMap
-    reportingUnitIdField = arcpy.GetParameterAsText(1)
-    inLandCoverGrid = arcpy.GetParameterAsText(2)
-    lccFilePath = arcpy.GetParameterAsText(4)
-    metricsToRun = arcpy.GetParameterAsText(5)
-    outTable = arcpy.GetParameterAsText(6)
-    processingCellSize = arcpy.GetParameterAsText(7)
-    snapRaster = arcpy.GetParameterAsText(8)
-    optionalFieldGroups = arcpy.GetParameterAsText(9)
-    thresholdValue = ""
-
-    ## For debugging    
-#    inReportingUnitFeature = "D:/ATTILA_Jackson/testzone/shpfiles/wtrshd.shp"
-#    reportingUnitIdField = "HUC"
-#    inLandCoverGrid = "D:/ATTILA_Jackson/testzone/grids/lc_mrlc"
-#    lccFilePath = "D:/ATTILA_Jackson/testzone/NLCD 2001.lcc"
-#    metricsToRun = "'for  (Forest)'"
-#    outTable = "D:/ATTILA_Jackson/testzone/testoutputs/File Geodatabase.gdb/qacheck"
-#    processingCellSize = "30.6618"
-#    snapRaster = "D:/ATTILA_Jackson/testzone/grids/lc_mrlc"
-#    optionalFieldGroups = "'QACHECK  -  Quality Assurance Checks';'METRICADD  -  Area for all land cover classes'"
-#    env.workspace = "D:/ATTILA_Jackson/testzone/testoutputs/Scratch"
-#    env.overwriteOutput = True
- 
+    inputArguments = parameters.getParametersAsText([0])
+    
+    landCoverProportions(*inputArguments)
+    
+    
+def landCoverProportions(inReportingUnitFeature, reportingUnitIdField, inLandCoverGrid, lccFilePath, metricsToRun, outTable, processingCellSize, snapRaster, optionalFieldGroups) :
     # Check out any necessary licenses
     arcpy.CheckOutExtension("spatial")
     
@@ -131,6 +114,7 @@ def main(argv):
                 # generate output field name
                 outputFName = fldParams[0]+mClassName+fldParams[1]
                 
+                thresholdValue = ""
                 # see if the provided field name is too long for the output table type
                 if len(outputFName) > maxFieldNameSize:
                     defaultFieldName = outputFName # keep track of the originally generated field name
@@ -157,7 +141,7 @@ def main(argv):
                 metricsFieldnameDict[mClassName] = outputFName
                     
         # create the specified output table
-        newTable = CreateMetricOutputTable(outTable,inReportingUnitFeature,reportingUnitIdField,metricsClassNameList,metricsFieldnameDict,fldParams,qaCheckFlds,addAreaFldParams)
+        newTable = common.CreateMetricOutputTable(outTable,inReportingUnitFeature,reportingUnitIdField,metricsClassNameList,metricsFieldnameDict,fldParams,qaCheckFlds,addAreaFldParams)
         
         # Process: Tabulate Area
         # set the snap raster environment so the rasterized polygon theme aligns with land cover grid cell boundaries
@@ -203,7 +187,7 @@ def main(argv):
             #    value for the grid code into a dictionary with the grid code as the key.
             # 2) Determine if the grid code is to be included into the reporting unit effective area sum
             # 3) Calculate the total grid area present in the reporting unit
-            valFieldsResults = ProcessTabAreaValueFields(TabAreaValueFields,TabAreaValues,tabAreaDict,tabAreaTableRow,excludedValues)
+            valFieldsResults = common.ProcessTabAreaValueFields(TabAreaValueFields,TabAreaValues,tabAreaDict,tabAreaTableRow,excludedValues)
             tabAreaDict = valFieldsResults[0]
             effectiveAreaSum = valFieldsResults[1]
             excludedAreaSum = valFieldsResults[2]
@@ -213,7 +197,7 @@ def main(argv):
                 # get the grid codes for this specified metric
                 metricGridCodesList = lccClassesDict[mClassName].uniqueValueIds
                 # get the class percentage area and it's actual area from the tabulate area table
-                metricPercentageAndArea = CalcMetricPercentArea(metricGridCodesList, tabAreaDict, effectiveAreaSum)
+                metricPercentageAndArea = common.CalcMetricPercentArea(metricGridCodesList, tabAreaDict, effectiveAreaSum)
                 # add the calculation to the output row
                 outTableRow.setValue(metricsFieldnameDict[mClassName], metricPercentageAndArea[0])
                 
@@ -281,142 +265,8 @@ def main(argv):
 
 
 
-def CalcMetricPercentArea(metricGridCodesList, tabAreaDict, effectiveAreaSum):
-    """ Calculates the percentage of the reporting unit effective area occupied by the metric class codes and their total area
     
-        DESCRIPTION
-        -----------
-        Retrieves stored area figures for each grid code associated with selected metric and sums them.
-        That number, divided by the total effective area within the reporting unit and multiplied by 100, gives the
-        percentage of the effective reporting unit that is occupied by the metric class. Both the percentage and the 
-        final area sum are returned.
-        
-        PARAMETERS
-        ----------
-        metricGridCodesList: list of all grid values assigned to a metric class in the lcc file (e.g., [41, 42, 43] for the forest class)
-        tabAreaDict: dictionary with the area value of each grid code in a reporting unit keyed to the grid code
-        effectiveAreaSum: sum of the area of all grid cells in the reporting unit with codes not tagged as excluded in the lcc file
-        
-        RETURNS
-        -------
-        Tuple:
-            float 1 - the percentage of the reporting unit effective area that is occupied by the metric class codes
-            float 2 - the sum of the area of metric class codes
-        
-    """
     
-    metricAreaSum = 0                         
-    for aValueID in metricGridCodesList:
-        metricAreaSum += tabAreaDict.get(aValueID, 0) #add 0 if the lcc defined value is not found in the grid
-    
-    if effectiveAreaSum > 0:
-        metricPercentArea = (metricAreaSum / effectiveAreaSum) * 100
-    else: # all values found in reporting unit are in the excluded set
-        metricPercentArea = 0
-        
-    return metricPercentArea, metricAreaSum
-
-
-def ProcessTabAreaValueFields(TabAreaValueFields, TabAreaValues, tabAreaDict, tabAreaTableRow, excludedValues):
-    """ Processes the 'VALUE_' fields in a supplied row object for exclusion/inclusion status in metric area/percentage calculations. 
-    
-        DESCRIPTION
-        -----------
-        1) Go through each 'VALUE_' field in the TabulateArea table one row at a time and put the area value
-           for each grid code into a dictionary with the grid code as the key.
-        2) Determine if the area for the grid code is to be included into the reporting unit effective area sum.
-        3) Keep a running total of effective and excluded area within the reporting unit. Added together, these 
-           area sums provide the total grid area present in the reporting unit. That value is used to calculate
-           the amount of overlap between the reporting unit polygon and the underlying land cover grid.
-           
-        PARAMETERS
-        ----------
-        TabAreaValueFields: a list of the 'VALUE_' fieldnames from a TabulateArea generated table
-        TabAreaValues: a list of integer grid code values generated from the 'VALUE_' fieldnames
-        tabAreaDict: an empty dictionary to be populated with a key:value pair of gridcode:area
-        tabAreaTableRow: the current row object from TabluateArea output table arcpy SearchCursor
-        excludedValues: a frozenset of values not to use when calculating the reporting unit effective area.
-        
-        RETURNS
-        -------
-        Tuple:
-            dictionary - dictionary with the area value of each grid code in a reporting unit keyed to the grid code
-            float 1 - sum of the area within the reporting unit to be included in the percentage calculations
-            float 2 - sum of the area within the reporting unit to be excluded from percentage calculations
-            
-    """
-    
-    excludedAreaSum = 0  #area of reporting unit not used in metric calculations e.g., water area
-    effectiveAreaSum = 0  #effective area of the reporting unit e.g., land area
-
-    for i, aFld in enumerate(TabAreaValueFields):
-        # store the grid code and it's area value into the dictionary
-        valKey = TabAreaValues[i]
-        valArea = tabAreaTableRow.getValue(aFld.name)
-        tabAreaDict[valKey] = valArea
-
-        #add the area of each grid value to the appropriate area sum i.e., effective or excluded area
-        if valKey in excludedValues:
-            excludedAreaSum += valArea
-        else:
-            effectiveAreaSum += valArea               
-                       
-    return (tabAreaDict, effectiveAreaSum, excludedAreaSum)
-
-
-def CreateMetricOutputTable(outTable, inReportingUnitFeature, reportingUnitIdField, metricsClassNameList, metricsFieldnameDict, fldParams, qaCheckFlds, addAreaFldParams):
-    """ Returns new empty table for ATtILA metric generation output with appropriate fields for selected metric
-    
-        DESCRIPTION
-        -----------
-        Creates an empty table with fields for the reporting unit id, all selected metrics with appropriate fieldname
-        prefixes and suffixes (e.g. pUrb, rFor30), and any selected optional fields (e.g., LC_Overlap)
-        
-        PARAMETERS
-        ----------
-        outTable: file name including path for the ATtILA output table
-        inReportingUnitFeature: CatalogPath to the input reporting unit layer
-        reportingUnitIdField: fieldname for the input reporting unit id field
-        metricsClassNameList: a list of metric ClassNames parsed from the 'Metrics to run' input (e.g., [for, agt, shrb, devt])
-        metricsFieldnameDict: a dictionary of ClassName keys with field name values (e.g., "unat":"UINDEX", "for":"pFor")
-        fldParams: list of parameters to generate the selected metric output field (i.e., [Fieldname_prefix, Fieldname_suffix, Field_type, Field_Precision, Field_scale])
-        qaCheckFlds: a list of filename parameter lists for one or more selected optional fields fieldname generation (e.g., optionalFlds = [["LC_Overlap","FLOAT",6,1]])
-        addAreaFldParams: a list of filename parameters for the optional Add Area Fields selection (e.g., ["_A","DOUBLE",15,0])
-        
-        RETURNS
-        -------
-        Table
-        
-    """
-    outTablePath, outTableName = os.path.split(outTable)
-        
-    # need to strip the dbf extension if the outpath is a geodatabase; 
-    # should control this in the validate step or with an arcpy.ValidateTableName call
-    newTable = arcpy.CreateTable_management(outTablePath, outTableName)
-    
-    # process the user input to add id field to output table
-    IDfield = arcpyutil.fields.getFieldByName(inReportingUnitFeature, reportingUnitIdField)
-    arcpy.AddField_management(newTable, IDfield.name, IDfield.type, IDfield.precision, IDfield.scale)
-                
-    # add metric fields to the output table.
-    [arcpy.AddField_management(newTable, metricsFieldnameDict[mClassName], fldParams[2], fldParams[3], fldParams[4])for mClassName in metricsClassNameList]
-
-    # add any optional fields to the output table
-    if qaCheckFlds:
-        [arcpy.AddField_management(newTable, qaFld[0], qaFld[1], qaFld[2]) for qaFld in qaCheckFlds]
-        
-    if addAreaFldParams:
-        [arcpy.AddField_management(newTable, metricsFieldnameDict[mClassName]+addAreaFldParams[0], addAreaFldParams[1], addAreaFldParams[2], addAreaFldParams[3])for mClassName in metricsClassNameList]
-         
-    # delete the 'Field1' field if it exists in the new output table.
-    arcpyutil.fields.deleteFields(newTable, ["field1"])
-    
-        
-    return newTable
-
-
-
-
 
 if __name__ == "__main__":
     main(sys.argv)
--- ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
+++ ToolboxSource/ATtILA2/scripts/LandCoverSlopeOverlap_ArcGIS.py
@@ -12,116 +12,67 @@
     Spatial Analyst Extension
 
 """
+from pylet.arcpyutil import parameters
 
-import arcpy, os, sys
-from arcpy import env
+import arcpy
+import sys
 from arcpy.sa import *
 from pylet import lcc
-from ATtILA2.metrics import fields as outFields
+
 
 # Check out any necessary licenses
 arcpy.CheckOutExtension("spatial")
 
 def main(argv):
     """ Start Here """
-    # Script arguments
-    Input_reporting_unit_feature_object = arcpy.GetParameter(0)
-    Reporting_unit_ID_field = arcpy.GetParameterAsText(1)
-    Input_land_cover_grid = arcpy.GetParameterAsText(2)
-    lccFilePath = arcpy.GetParameterAsText(4)
-    Metrics_to_run = arcpy.GetParameterAsText(5)
-    Input_slope_grid = arcpy.GetParameterAsText(6)
-    Input_slope_threshold_value = arcpy.GetParameterAsText(7)
-    Output_table = arcpy.GetParameterAsText(8)
-    Processing_cell_size = arcpy.GetParameterAsText(9)
-    Snap_raster = arcpy.GetParameterAsText(10)
-    Optional_field_groups = arcpy.GetParameterAsText(11)
     
-    # the variables row and rows are initially set to None, so that they can
-    # be deleted in the finally block regardless of where (or if) script fails
-    outTable_row, outTable_rows = None, None
-    tabAreaTable_row, tabAreaTable_rows = None, None
+    inputArguments = parameters.getParametersAsText([0])
     
-    # get current snap environment to restore at end of script
-    tempEnvironment0 = env.snapRaster
-    # set the snap raster environment so the AG on Steep Slopes grid aligns with land cover grid cell boundaries
-    env.snapRaster = Snap_raster
-    env.cellSize = Processing_cell_size
-    
-    
-    try:
-        # XML Land Cover Coding file loaded into memory
-        lccObj = lcc.LandCoverClassification(lccFilePath)
-        # Get the lccObj values dictionary to determine if a grid code is to be included in the effective reporting unit area calculation
-        lccValuesDict = lccObj.values
-        # get the frozenset of excluded values (i.e., values not to use when calculating the reporting unit effective area)
-        excludedValues = lccValuesDict.getExcludedValueIds()
-        
-        # Generate the slope X land cover gird where areas below the threshold slope are
-        # set to the value 'Maximum Land Cover Class Value + 1'.
-        LCGrid = Raster(Input_land_cover_grid)
-        SLPGrid = Raster(Input_slope_grid)
-        AreaBelowThresholdValue = int(LCGrid.maximum + 1)
-        where_clause = "VALUE >= "+Input_slope_threshold_value
-        SLPxLCGrid = Con(SLPGrid, LCGrid, AreaBelowThresholdValue, where_clause)
+    landCoverOnSlopeProportions(*inputArguments)
 
-        # if certain land cover codes are tagged as 'excluded = TRUE', generate grid where land cover codes are 
-        # preserved for areas coincident with steep slopes, areas below the slope threshold are coded with the 
-        # AreaBelowThresholdValue except for where the land cover code is included in the excluded values list.
-        # In that case, the excluded land cover values are maintained in the low slope areas.
-        
-        if excludedValues:
-            # build a whereClause string (e.g. "VALUE = 11 or VALUE = 12") to identify where on the land cover grid excluded values occur
-            whereExcludedClause = "VALUE = " + " or VALUE = ".join([str(item) for item in excludedValues])
+def landCoverOnSlopeProportions(inReportingUnitFeature, reportingUnitIdField, inLandCoverGrid, lccFilePath, 
+                                metricsToRun, inSlopeGrid, inSlopeThresholdValue, outTable, processingCellSize, 
+                                snapRaster, optionalFieldGroups):
 
-            SLPxLCGrid = Con(LCGrid, LCGrid, SLPxLCGrid, whereExcludedClause)
-            
-        # save the file if intermediate products outputs are checked
-        SLPxLCGrid.save(arcpy.CreateUniqueName("slxlc"))
-
-        
-        
-    except arcpy.ExecuteError:
-        arcpy.AddError(arcpy.GetMessages(2))
-        
-    except:
-        import traceback
-        # get the traceback object
-        tb = sys.exc_info()[2]
-        tbinfo = traceback.format_tb(tb)[0]
-        
-        # Concatenate information together concerning the error into a message string
-        
-        pymsg = "PYTHON ERRORS:\nTraceback info:\n" + tbinfo + "\nError Info:\n" + str(sys.exc_info()[1])
-        msgs = "ArcPy ERRORS:\n" + arcpy.GetMessages(2) + "\n"
+    # XML Land Cover Coding file loaded into memory
+    lccObj = lcc.LandCoverClassification(lccFilePath)
     
-        # Return python error messages for use in script tool
-        
-        arcpy.AddError(pymsg)
-        arcpy.AddError(msgs)
-        
-    finally:
-        # delete cursor and row objects to remove locks on the data
-        if outTable_row: del outTable_row
-        if outTable_rows: del outTable_rows
-        if tabAreaTable_rows: del tabAreaTable_rows
-        if tabAreaTable_row: del tabAreaTable_row
-            
-        # restore the environments
-        env.snapRaster = tempEnvironment0
-        
-        # return the spatial analyst license    
-        arcpy.CheckInExtension("spatial")
-        
-        print "Finished."
-        
+    SLPxLCGrid = getIntersectOfGrids(lccObj,inLandCoverGrid, inSlopeGrid, inSlopeThresholdValue)
+    
+    # save the file if intermediate products outputs are checked
+    #SLPxLCGrid.save(arcpy.CreateUniqueName("slxlc"))
+    
+    
 
-def ParseCheckboxSelections(selectionsString):
-    """ Returns a list of the items selected by the user.
-        The expected input is a string with the following format: 'item<two spaces>description';'item<two spaces>description';'item...'
-        e.g., the string, 'for  [pfor] Forest';'wetl  [pwetl]  wetland', becomes the list, ['for','wetl']
-    """
-    return map((lambda splitItemAndDesc: splitItemAndDesc.split('  ')[0]), selectionsString.replace("'","").split(';'))
+def getIntersectOfGrids(lccObj,inLandCoverGrid, inSlopeGrid, inSlopeThresholdValue):
+    
+    # Get the lccObj values dictionary to determine if a grid code is to be included in the effective reporting unit area calculation    
+    lccValuesDict = lccObj.values
+    
+    # get the frozenset of excluded values (i.e., values not to use when calculating the reporting unit effective area)
+    excludedValues = lccValuesDict.getExcludedValueIds()
+    
+    # Generate the slope X land cover gird where areas below the threshold slope are
+    # set to the value 'Maximum Land Cover Class Value + 1'.
+    LCGrid = Raster(inLandCoverGrid)
+    SLPGrid = Raster(inSlopeGrid)
+    AreaBelowThresholdValue = int(LCGrid.maximum + 1)
+    where_clause = "VALUE >= " + inSlopeThresholdValue
+    SLPxLCGrid = Con(SLPGrid, LCGrid, AreaBelowThresholdValue, where_clause)
+    
+    # if certain land cover codes are tagged as 'excluded = TRUE', generate grid where land cover codes are 
+    # preserved for areas coincident with steep slopes, areas below the slope threshold are coded with the 
+    # AreaBelowThresholdValue except for where the land cover code is included in the excluded values list.
+    # In that case, the excluded land cover values are maintained in the low slope areas.
+    
+    if excludedValues:
+        # build a whereClause string (e.g. "VALUE = 11 or VALUE = 12") to identify where on the land cover grid excluded values occur
+        whereExcludedClause = "VALUE = " + " or VALUE = ".join([str(item) for item in excludedValues])
+    
+        SLPxLCGrid = Con(LCGrid, LCGrid, SLPxLCGrid, whereExcludedClause)
+    
+    return SLPxLCGrid
+
     
     
 if __name__ == "__main__":
