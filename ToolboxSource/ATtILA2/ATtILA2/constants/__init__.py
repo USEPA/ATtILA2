255660155d78e9c38fe5ae37ba2fb9f864db01e4
Michael Jackson <jackson.michael@epa.gov>
2012-04-24 20:26:00 -0700

    constants reorganized

--- ToolboxSource/ATtILA2/ATtILA2/constants/__init__.py
+++ ToolboxSource/ATtILA2/ATtILA2/constants/__init__.py
@@ -1,5 +1,31 @@
-import metricConstants
-import fieldConstants
+import metricConstants as mc
+import applicationConstants as ac
+import ATtILA2
+import sys
+
+def getMetricContantsFromFilePath(filePath=None, delimiter="_"):
+    """ Parses full file path to look up constants
+        
+        No arguments are required.  If filePath is not given, it uses sys.argv[0] (full path to executed script)
+        
+        filePath:  full file path from which basename is split by delimiter, first item in split is used
+        delimiter: character(s) by which basename is split
+        
+    """
+    
+    if not filePath:
+        filePath = sys.argv[0]
+    
+    key = ATtILA2.utils.files.getKeyFromFilePath(filePath, delimiter)
+    
+    if key == mc.rpConstants.name:
+        return mc.rpConstants()
+    
+    if key == mc.lcospConstants.name:
+        return mc.lcospConstants
+    
+    if key == mc.lcpConstants.name:
+        return mc.lcpConstants
 
 
 
---
+++ ToolboxSource/ATtILA2/ATtILA2/constants/applicationConstants.py
@@ -0,0 +1,24 @@
+
+# Common Variables
+tbxSourceFolderName = "ToolboxSource"
+noFeaturesMessage = "Dataset exists, but there are no features (zero rows)"
+inputIdFieldTypes = ["SmallInteger", "Integer", "String"]
+userOption = "User Defined"
+optionalFieldsName = "Additional Options"
+descriptionDelim = "  -  "
+areaFieldParameters = ["_A","DOUBLE",15,0]
+srcFolderSuffix = ".src"
+tbxFileSuffix = ".tbx"
+tbxFileDelim = "__"
+tbxSriptToolDelim = "#"
+metricDescription = "{0}" + descriptionDelim + "[{1}]  {2}"
+qaCheckName = "QAFIELDS"
+metricAddName = "AREAFIELDS"
+intermediateName = "INTERMEDIATES"
+qaCheckDescription = "{0}{1}Add Quality Assurance Fields".format(qaCheckName, descriptionDelim)
+metricAddDescription = "{0}{1}Add Area Fields for All Land Cover Classes".format(metricAddName, descriptionDelim)
+intermediateDescription = "{0}{1}Retain Intermediate Layers Generated During Metric Calculation".format(intermediateName, descriptionDelim)
+defaultDecimalFieldType = "FLOAT"
+defaultIntegerFieldType = "SHORT"
+defaultAreaFieldType = "DOUBLE"
+fieldOverrideName = "Field"
