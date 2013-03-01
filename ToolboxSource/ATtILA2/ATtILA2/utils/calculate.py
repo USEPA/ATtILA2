d6e79f453f389a1082661e7ed453950db58e51e7
Ellen D'Amico <damico.ellen@epa.gov>
2013-03-01 15:49:04 -0500

    Updates calculate script with code to run math calculations for Landcover Diversity Metric

--- ToolboxSource/ATtILA2/ATtILA2/utils/calculate.py
+++ ToolboxSource/ATtILA2/ATtILA2/utils/calculate.py
@@ -356,3 +356,82 @@ def lineDensityCalculator(inLines,inAreas,areaUID,unitArea,outLines,densityField
         iaField = vector.addCalculateField(outLines,iaField,calcExpression,codeblock)
     
     return outLines, lineLengthFieldName
+
+def createValueDict(alist):
+    valuedict={}
+    for a in alist:
+        valuedict[(int(a.split(":")[0]))] = float(a.split(":")[1])
+    return valuedict
+
+ResultsDict = {}
+def createTotalTable(tabAreaTable, zoneAreaDict):
+    TotalAreaDict = {}
+    CopyDict = {}
+    OverlapDict = {}
+    for tabAreaTableRow in tabAreaTable:
+        alist = []
+        TotalArea = 0
+        #Calculate Total Area Dictionary
+        for k in tabAreaTableRow.tabAreaDict.keys():
+            TotalArea = TotalArea + tabAreaTableRow.tabAreaDict[k]
+            alist.append(str(k) + ":" + str(tabAreaTableRow.tabAreaDict[k]))
+
+        TotalAreaDict[tabAreaTableRow.zoneIdValue] = TotalArea
+        CopyDict[tabAreaTableRow.zoneIdValue]= alist
+        # if QAField option is selected calculate overlap
+        if zoneAreaDict:
+            zoneArea = zoneAreaDict[tabAreaTableRow.zoneIdValue]
+            overlapCalc = (zoneArea/TotalArea) * 100
+            OverlapDict[tabAreaTableRow.zoneIdValue] = overlapCalc
+    #Calculate proportional dictionary
+    createProportionsDict(CopyDict,TotalAreaDict)
+
+    return ResultsDict, OverlapDict
+
+
+#Calculate Proportions
+def createProportionsDict(CopyDict, TotalAreaDict):
+    import math
+    ProportionsDict = {}
+
+    for k in CopyDict.keys():
+        indivrowdict = {}
+        pSum = 0
+        S = 0
+        C = 0
+        indivrowdict = createValueDict(CopyDict[k])
+        for i in indivrowdict.keys():
+            if indivrowdict[i] >0:
+                #Calculate the percent land use for use in further calculations
+                P = indivrowdict[i] / TotalAreaDict[k]
+                #Calculate the sum for use in the Shannon-Weiner calculations
+                pSum = pSum + (P * math.log(P))
+                #Calculate the Simpson Index
+                C = C + P * P
+                #Calculate the simple diversity
+                S = S + 1
+                #Population the proportion dictionary
+                ProportionsDict[k] =  str(pSum) + "," + str(C) + "," + str(S)
+
+    #Calculate final results to be reported out
+    for k in ProportionsDict.keys():
+        #Calculate Shannon-Weiner Diversity equation (H)
+        H = float(ProportionsDict[k].split(",") [0])* -1
+
+        #Calculate Simple Diversity equation
+        S = int(ProportionsDict[k].split(",")[2])
+        
+        #Calculate Shannon-Weiner Diversity equation (H prime)
+        Hprime = H/math.log(S)
+        
+        #Calculate Simpson Diversity equation
+        C = float(ProportionsDict[k].split(",")[1])
+        
+        #Create Results Dictionary
+        ResultsDict[k] = str(H) + "," + str(Hprime) + "," + str(S) + "," + str(C)
+
+
+
+    return ResultsDict
+
+        
