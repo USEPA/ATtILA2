a5f5876d83407acf4d06fd705d4a821b12e90ab3
Ellen D'Amico <damico.ellen@epa.gov>
2013-04-04 08:30:39 -0400

    Test script to run CoreEdge metric within Eclipse

--- ToolboxSource/ATtILA2/tests/UnitTests/CoreEdgeTest.py
+++ ToolboxSource/ATtILA2/tests/UnitTests/CoreEdgeTest.py
@@ -10,6 +10,8 @@ import arcpy
 import ATtILA2
 import parameters_eid as p
 import outputValidation
+from arcpy import env
+env.overwriteOutput = True
 
 def runTest():
     try:
@@ -26,6 +28,34 @@ def runTest():
 #        
 #        print results
     
+    except Exception, e: 
+        ATtILA2.errors.standardErrorHandling(e)
+
+def runTableTest():
+    import os
+    from ATtILA2 import setupAndRestore
+    from ATtILA2 import utils
+    from ATtILA2.constants import metricConstants
+    from pylet import lcc
+    try:
+        if arcpy.Exists(p.outTable):
+            arcpy.Delete_management(p.outTable)
+        print "Running Table Test calculation"
+        metricsBaseNameList, optionalGroupsList = setupAndRestore.standardSetup(p.snapRaster, p.processingCellSize,
+                                                                                 os.path.dirname(p.outTable),
+                                                                                 [p.metricsToRun,p.optionalFieldGroups] )
+        lccObj = lcc.LandCoverClassification(p.lccFilePath)
+        metricConst = metricConstants.caeamConstants()
+        outIdField = utils.settings.getIdOutField(p.inReportingUnitFeature, p.reportingUnitIdField)
+        for m in metricsBaseNameList:
+            Core = m + "_Core"
+            Edge = m + "_Edge"
+            optionalGroupsList.append(Core)
+            optionalGroupsList.append(Edge)
+        newtable, metricsFieldnameDict = utils.table.tableWriterByClass(p.outTable, metricsBaseNameList,optionalGroupsList,
+                                                                        metricConst, lccObj, outIdField)
+        print metricsFieldnameDict  
+             
     except Exception, e:
         ATtILA2.errors.standardErrorHandling(e)
         
