ccba4a6fffeab28af9b709f1cec6576a66b7a7aa
Torrin Hultgren <hultgren.torrin@epa.gov>
2016-01-29 18:06:02 -0500

    Minor wording changes

--- ToolboxSource/ATtILA2/tests/UnitTests/landCoverProportionsTest.py
+++ ToolboxSource/ATtILA2/tests/UnitTests/landCoverProportionsTest.py
@@ -1,30 +1,26 @@
 '''
-Test to evaluate consistency of Land Cover Proportion Calculation
+Test to evaluate consistency of LandCoverProportion Calculation
 
-Created July 2012
+Created on Jul 3, 2012
 
 @author: thultgren Torrin Hultgren, hultgren.torrin@epa.gov
 '''
 
 import arcpy
 import ATtILA2
+import filecmp
 import parameters as p
-import outputValidation
 
 def runTest():
     try:
         if arcpy.Exists(p.outTable):
             arcpy.Delete_management(p.outTable)
-            
-        print "Running LandCoverProportion calculation"
         ATtILA2.metric.runLandCoverProportions(p.inReportingUnitFeature, p.reportingUnitIdField, p.inLandCoverGrid, 
-                                               p._lccName, p.lccFilePath, p.metricsToRun_LCP, p.outTable, 
-                                               p.processingCellSize, p.snapRaster, p.optionalFieldGroups)
+                                               p._lccName, p.lccFilePath, p.metricsToRun, p.outTable, p.processingCellSize, 
+                                               p.snapRaster, p.optionalFieldGroups)
     
-        print "Testing LandCoverProportion results"
-        results = outputValidation.compare(p.refLandCoverOutput,p.outTable)
-        
-        print results
+        result = filecmp.cmp(p.outTable, p.testLandCoverOutput, False)
+        print result
     
     except Exception, e:
         ATtILA2.errors.standardErrorHandling(e)
