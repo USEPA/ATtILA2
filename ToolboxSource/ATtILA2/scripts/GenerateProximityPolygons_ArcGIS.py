46734e32c02495068823b64e3c6aa991f710b712
donebert <ebert.donald@epa.gov>
2020-11-09 12:01:22 -0500

    Script for launching the Generate Proximity Polygons utility

---
+++ ToolboxSource/ATtILA2/scripts/GenerateProximityPolygons_ArcGIS.py
@@ -0,0 +1,20 @@
+""" Generate Proximity Polygons
+
+    This script is associated with an ArcToolbox script tool.
+"""
+
+import sys
+from ATtILA2 import metric
+from ATtILA2.utils import parameters
+
+
+def main(_argv):
+    
+    # Script arguments
+    inputArguments = parameters.getParametersAsText([0])
+    
+    metric.runCoreAndEdgeMetrics(*inputArguments)
+    
+    
+if __name__ == "__main__":
+    main(sys.argv)
