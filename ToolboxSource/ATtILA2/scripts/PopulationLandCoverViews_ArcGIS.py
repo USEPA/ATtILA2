132e83ea059b69b40087216e68e8d03c08ef8e97
donebert <ebert.donald@epa.gov>
2021-02-17 11:54:54 -0500

    Script to run with the Population Land Cover Views tool

---
+++ ToolboxSource/ATtILA2/scripts/PopulationLandCoverViews_ArcGIS.py
@@ -0,0 +1,20 @@
+""" Population Land Cover Views Metrics
+
+    This script is associated with an ArcToolbox script tool.
+    
+"""
+
+
+import sys
+from ATtILA2 import metric
+from ATtILA2.utils import parameters
+
+def main(_argv):
+    
+    # Script arguments
+    inputArguments = parameters.getParametersAsText([0, 2, 8])
+    
+    metric.runPopulationLandCoverViews(*inputArguments)
+
+if __name__ == "__main__":
+    main(sys.argv)
